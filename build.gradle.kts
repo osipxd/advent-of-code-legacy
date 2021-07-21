71dd99f9b70274f6c205584e8c7a1e968d3ade97
Osip Fatkullin <osip.fatkullin@gmail.com>
2021-07-21 22:19:14 +0300

    Add common project configuration

---
+++ build.gradle.kts
@@ -0,0 +1,21 @@
+plugins {
+    kotlin("jvm") version "1.5.21" apply false
+}
+
+subprojects {
+    apply(plugin = "kotlin")
+
+    dependencies {
+        "testImplementation"(kotlin("test-junit5"))
+        "testImplementation"("org.junit.jupiter:junit-jupiter-api:5.7.2")
+        "testRuntimeOnly"("org.junit.jupiter:junit-jupiter-engine")
+    }
+
+    repositories {
+        mavenCentral()
+    }
+
+    tasks.named<Test>("test") {
+        useJUnitPlatform()
+    }
+}
