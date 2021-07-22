e78b4816caca39f5456945d0d9c34da2b343262d
Osip Fatkullin <osip.fatkullin@gmail.com>
2021-07-22 11:18:45 +0300

    d1: Refactor tests

--- 2020/src/test/kotlin/Day1Test.kt
+++ 2020/src/test/kotlin/Day1Test.kt
@@ -3,48 +3,29 @@ import kotlin.test.assertEquals
 
 class Day1Test {
 
-    private val exampleExpenseReport = listOf(
-        1721,
-        979,
-        366,
-        299,
-        675,
-        1456,
-    )
+    private val exampleExpenseReport: List<Int>
+        get() = listOf(1721, 979, 366, 299, 675, 1456)
+
+    private val inputExpenseReport: List<Int>
+        get() = readNumbers("day1.txt")
 
     @Test
     fun `part1Naive - expense report from example`() {
-        // Given
-        val expenseReport = exampleExpenseReport
-
-        // Expect
-        assertEquals(expected = 514579, actual = Day1.part1Naive(expenseReport))
+        assertEquals(expected = 514579, actual = Day1.part1Naive(exampleExpenseReport))
     }
 
     @Test
     fun `part1Naive - expense report from input`() {
-        // Given
-        val expenseReport = readNumbers("day1.txt")
-
-        // Then
-        assertEquals(expected = 858496, actual = Day1.part1Naive(expenseReport))
+        assertEquals(expected = 858496, actual = Day1.part1Naive(inputExpenseReport))
     }
 
     @Test
     fun `part2Naive - expense report from example`() {
-        // Given
-        val expenseReport = exampleExpenseReport
-
-        // Expect
-        assertEquals(expected = 241861950, actual = Day1.part2Naive(expenseReport))
+        assertEquals(expected = 241861950, actual = Day1.part2Naive(exampleExpenseReport))
     }
 
     @Test
     fun `part2Naive - expense report from input`() {
-        // Given
-        val expenseReport = readNumbers("day1.txt")
-
-        // Expect
-        assertEquals(expected = 263819430, actual = Day1.part2Naive(expenseReport))
+        assertEquals(expected = 263819430, actual = Day1.part2Naive(inputExpenseReport))
     }
 }
