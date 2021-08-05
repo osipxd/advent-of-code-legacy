ee9c8ae6731af0d683387aa57b4502ee91f3a1a1
Osip Fatkullin <osip.fatkullin@gmail.com>
2021-08-05 19:33:11 +0300

    d5p1: Solution

---
+++ 2020/src/test/kotlin/Day5Test.kt
@@ -0,0 +1,24 @@
+import org.junit.jupiter.api.Test
+import kotlin.test.assertEquals
+
+internal class Day5Test {
+
+    private val example: List<String> = listOf(
+        "BFFFBBFRRR",
+        "FFFBBBFRRR",
+        "BBFFBBFRLL",
+    )
+
+    private val input: List<String>
+        get() = readLines("day5.txt")
+
+    @Test
+    fun `part1 - example`() {
+        assertEquals(820, Day5.part1(example))
+    }
+
+    @Test
+    fun `part1 - input`() {
+        assertEquals(871, Day5.part1(input))
+    }
+}
--- build.gradle.kts
+++ build.gradle.kts
@@ -9,6 +9,7 @@ subprojects {
         "testImplementation"(project(":utils"))
         "testImplementation"(kotlin("test-junit5"))
         "testImplementation"("org.junit.jupiter:junit-jupiter-api:5.7.2")
+        "testImplementation"("org.junit.jupiter:junit-jupiter-params:5.7.2")
         "testRuntimeOnly"("org.junit.jupiter:junit-jupiter-engine")
     }
 
