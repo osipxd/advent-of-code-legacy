97425bc0bc88972f5b6a2a47d0c7dfb2982b7ff3
Osip Fatkullin <osip.fatkullin@gmail.com>
2021-12-06 10:12:19 +0300

    d2p1: Solution

---
+++ 2021/src/test/kotlin/Day2Test.kt
@@ -0,0 +1,28 @@
+import kotlin.test.Test
+import kotlin.test.assertEquals
+
+internal class Day2Test {
+
+    private val example: Sequence<String>
+        get() = sequenceOf(
+            "forward 5",
+            "down 5",
+            "forward 8",
+            "up 3",
+            "down 8",
+            "forward 2",
+        )
+
+    private val input: Sequence<String>
+        get() = readLineSequence("day2.txt")
+
+    @Test
+    fun `part1 - example`() {
+        assertEquals(expected = 150, actual = Day2.part1(example))
+    }
+
+    @Test
+    fun `part1 - input`() {
+        assertEquals(expected = 1459206, actual = Day2.part1(input))
+    }
+}
